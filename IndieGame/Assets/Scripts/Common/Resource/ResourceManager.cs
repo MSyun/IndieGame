f29614b696686e7f9193fc58e1d607f117ba2067
Syun <syun7game8fight7@yahoo.co.jp>
2019-04-12 02:04:41 +0900

    Resource用の読み込み、削除機能の追加(一旦Coroutineでの実装

---
+++ IndieGame/Assets/Scripts/Common/Resource/ResourceManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MSyun.Common.Resource {
+	public class ResourceManager : MonoBehaviour {
+		public T Load<T>(string path) where T : Object {
+			return Resources.Load<T>(path);
+		}
+
+		public T LoadAsync<T>(string path) where T : Object {
+			T data = null;
+			StartCoroutine(this.LoadAsyncEnum<T>(path, data));
+			return data;
+		}
+
+		public void Unload(Object asset) {
+			Resources.UnloadAsset(asset);
+		}
+
+		private IEnumerator LoadAsyncEnum<T>(string path, T data) where T : Object {
+			var request = Resources.LoadAsync<T>(path);
+
+			while (!request.isDone) {
+				Debug.Log("Loading progress:" + request.progress.ToString());
+				yield return null;
+			}
+
+			data = request.asset as T;
+		}
+	}
+}
