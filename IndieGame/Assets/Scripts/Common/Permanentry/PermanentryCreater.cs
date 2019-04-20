1fedf888e2c724f24c5f63f65248d7a39b4397b3
Syun <syun7game8fight7@yahoo.co.jp>
2019-04-21 01:47:36 +0900

    常駐シーン生成クラスを追加

---
+++ IndieGame/Assets/Scripts/Common/Permanentry/PermanentryCreater.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MSyun.Common.Permanentry {
+	using Scene;
+
+	public class PermanentryCreater : MonoBehaviour {
+		private void Awake() {
+			if (PermanentryManager.Instance != null)
+				return;
+
+			SceneController.LoadScene((int)SceneName.Scene.PERMANENTRY, true);
+		}
+	}
+}
