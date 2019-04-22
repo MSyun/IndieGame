8f685e4c1efe3166fce366b69bdf1b0ae37440c8
Syun <syun7game8fight7@yahoo.co.jp>
2019-04-23 02:10:19 +0900

    fadeに最低限の機能を追加

--- IndieGame/Assets/Scripts/Common/Scene/Fade/ISceneFade.cs
+++ IndieGame/Assets/Scripts/Common/Scene/Fade/ISceneFade.cs
@@ -1,10 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UniRx.Async;
+using System.Threading.Tasks;
+using UnityEngine.UI;
 
 namespace MSyun.Common.Scene.Fade {
 	public abstract class ISceneFade {
-		public abstract void FadeIn();
-		public abstract void FadeOut();
+
+		protected UnityEvent onBeginFadeIn = new UnityEvent();
+		protected UnityEvent onEndFadeIn = new UnityEvent();
+		protected UnityEvent onBeginFadeOut = new UnityEvent();
+		protected UnityEvent onEndFadeOut = new UnityEvent();
+
+		public void AddBeginFadeInEvent(UnityAction func) {
+			onBeginFadeIn.AddListener(func);
+		}
+
+		public void RemoveBeginFadeInEvent(UnityAction func) {
+			onBeginFadeIn.RemoveListener(func);
+		}
+
+		public void AddEndFadeInEvent(UnityAction func) {
+			onEndFadeIn.AddListener(func);
+		}
+
+		public void RemoveEndFadeInEvent(UnityAction func) {
+			onEndFadeIn.RemoveListener(func);
+		}
+
+		public void AddBeginFadeOutEvent(UnityAction func) {
+			onBeginFadeOut.AddListener(func);
+		}
+
+		public void RemoveBeginFadeOutEvent(UnityAction func) {
+			onBeginFadeOut.RemoveListener(func);
+		}
+
+		public void AddEndFadeOutEvent(UnityAction func) {
+			onEndFadeOut.AddListener(func);
+		}
+
+		public void RemoveEndFadeOutEvent(UnityAction func) {
+			onEndFadeOut.RemoveListener(func);
+		}
+
+		public void FadeIn(float seconds = 1.0f) {
+			this.onBeginFadeIn.Invoke();
+			this.onBeginFadeIn.RemoveAllListeners();
+
+			this.UniqueFadeIn(seconds);
+
+			this.onEndFadeIn.Invoke();
+			this.onEndFadeIn.RemoveAllListeners();
+		}
+
+		public void FadeOut(float seconds = 1.0f) {
+			this.onBeginFadeOut.Invoke();
+			this.onBeginFadeOut.RemoveAllListeners();
+
+			this.UniqueFadeOut(seconds);
+
+			this.onEndFadeOut.Invoke();
+			this.onEndFadeOut.RemoveAllListeners();
+		}
+
+		public void Update() {
+
+		}
+
+		protected abstract void UniqueFadeIn(float seconds);
+		protected abstract void UniqueFadeOut(float seconds);
 	}
 }
--- IndieGame/Assets/Scripts/Common/Scene/Fade/SampleSceneFade.cs
+++ IndieGame/Assets/Scripts/Common/Scene/Fade/SampleSceneFade.cs
@@ -1,15 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UniRx.Async;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MSyun.Common.Scene.Fade {
 	public sealed class SampleSceneFade : ISceneFade {
-		public override void FadeIn() {
-			
+
+		private Image image;
+
+		public void SetImage(Image image) {
+			this.image = image;
 		}
 
-		public override void FadeOut() {
-			
+		protected override void UniqueFadeIn(float seconds) {
+			float curTime = 0.0f;
+			Color color = image.color;
+			while (seconds > curTime) {
+				curTime += 0.01f;
+
+				color.a = Mathf.Lerp(0.0f, 1.0f, seconds / curTime);
+				image.color = color;
+			}
+		}
+
+		protected override void UniqueFadeOut(float seconds) {
+			float curTime = 0.0f;
+			Color color = image.color;
+			while (seconds > curTime) {
+				curTime += 0.01f;
+
+				color.a = Mathf.Lerp(1.0f, 0.0f, seconds / curTime);
+				image.color = color;
+			}
 		}
 	}
 }
