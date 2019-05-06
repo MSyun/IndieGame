93031ad1941973b8b2f5f27233b2cd8a2c0386c4
Syun <syun7game8fight7@yahoo.co.jp>
2019-05-06 16:42:02 +0900

    シーン初期化時のInputの設定を用意

--- IndieGame/Assets/Scripts/Common/Permanentry/PermanentryCreater.cs
+++ IndieGame/Assets/Scripts/Common/Permanentry/PermanentryCreater.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 namespace MSyun.Common.Permanentry {
 	using UnityEngine.SceneManagement;
 	using Scene;
+	using Input;
 
 	public class PermanentryCreater : MonoBehaviour {
+
+		[SerializeField]
+		private InputType initializeInputType = InputType.System;
+
 		private void Awake() {
 			if (PermanentryManager.Instance != null)
 				return;
 
 			SceneManager.LoadScene((int)SceneName.Scene.PERMANENTRY, LoadSceneMode.Additive);
+		}
+
+		private void Start() {
+			PermanentryManager.Instance.GameController.SetType(this.initializeInputType);
+
+			Destroy(this.gameObject);
 		}
 	}
 }
